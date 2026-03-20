6f2d93dc7f4221fa92cdcdca41a221430b40b717
Rafael <rafangarac@gmail.com>
2026-03-20 17:42:37 -0500

    Create SpringProfiles to difference DEV and PROD

--- users/users/src/main/java/com/pr_rg/users/UsersApplication.java
+++ users/users/src/main/java/com/pr_rg/users/UsersApplication.java
@@ -1,13 +1,20 @@
 package com.pr_rg.users;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UsersApplication {
+public class UsersApplication implements CommandLineRunner {
 
+	@Value("${custom.log.message}")
+	private String logMessage;
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
 	}
-
+	@Override
+	public void run(String... args) {
+		System.out.println(logMessage);
+	}
 }
